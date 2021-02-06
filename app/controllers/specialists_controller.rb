c635c9d8ba556903987703057fd21c0e6471a45e
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-02-06 20:03:23 +0100

    create cat and specialists

--- app/controllers/specialists_controller.rb
+++ app/controllers/specialists_controller.rb
@@ -1,22 +1,47 @@
 class SpecialistsController < ApplicationController
   def index
+    @specialists = Specialist.all
   end
 
   def show
-  end
-
-  def create
+    # @category = Category.find(params[:category_id])
+    @specialist = Specialist.find(params[:id])
   end
 
   def new
+    @specialist = Specialist.new
+    @category = Category.find(params[:category_id])
   end
 
-  def update
+  def create
+    @specialist = Specialist.create(specialist_params)
+    @specialist.category = Category.find(params[:category_id])
+    if @specialist.save
+      redirect_to category_path(@specialist.category), notice: 'Produit créé.'
+    else
+      render :new
+    end
   end
 
   def edit
+    @specialist = Specialist.find(params[:id])
+  end
+
+  def update
+    @specialist = Specialist.find(params[:id])
+    @specialist.update(specialist_params)
+    redirect_to category_path
   end
 
   def destroy
+    @specialist = Specialist.find(params[:id])
+    @specialist.destroy
+    redirect_to category_path
+  end
+
+  private
+
+  def specialist_params
+    params.require(:specialist).permit(:lastname, :firstname, :profession, :description, :rating, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   
-  get 'consultations/index'
-  get 'consultations/show'
-  get 'consultations/create'
-  get 'consultations/new'
-  get 'consultations/update'
-  get 'consultations/edit'
-  get 'consultations/destroy'
+  
   devise_for :users
   root to: 'categories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :categories do 
-    resources :specialists
+    resources :specialists, only: [:index, :update, :new, :create]
   end
+
+  scope shallow_prefix: "sekret" do
+    resources :specialists, only: [:edit, :show], shallow: true
+  end
+
+
 end

e514a323debccbc1eea1b9905bb20d0e79016a38
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-02-11 07:28:19 +0100

    front

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :categories do 
     resources :specialists, only: [:index, :show, :update, :new, :create] do
-      resources :consultations, only: [:index, :new, :create, :show] 
+      resources :consultations, only: [:new, :create, :show] 
     end
   end
 
-  resources :consultations, only: [:destroy] 
+  resources :consultations, only: [:index, :destroy] 
   resources :specialist, only: [:destroy]
   
 

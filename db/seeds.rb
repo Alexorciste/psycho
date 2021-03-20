1e589f82954194b9832f83b01b94574a0d6a19f4
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-03-20 23:18:02 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@ cat2 = URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615965499/j
 cat3 = URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615965501/kcn6uhdmjhifrqbpfk1brz8uy1zl.png")
 
 Category.destroy_all
-Category.create(name: "Service Juridique", description: "Vous avez signé un pacte avec une entité Démoniaque mais vous n'êtes plus certain de vouloir aller au bout ? Pas d'inquiétude, notre service juridique vous accompagne dans toutes vos démarches.").photo.attach(io: cat1, filename: 'devil.png', content_type: 'image/png')
+Category.create(name: "Service Juridique", description: "Vous avez signé un pacte avec une entité Démoniaque mais vous n'êtes plus certain de vouloir aller au bout ? Pas d'inquiétude, notre service juridique vous accompagne dans toutes vos démarches.").photo.attach(io: cat1, filename: 'devil.jpg', content_type: 'image/jpg')
 Category.create(name: "Expertise médicale", description: "Possession Démoniaque ou léger accès schizophrénique, véritable Poltergeist ou Délirium tremens bénin ? Notre équipe médicale est là pour vous aider à y voir clair.").photo.attach(io: cat2, filename: 'west.jpg', content_type: 'image/jpg')
-Category.create(name: "Désinfection", description: "Une entité Maléfique a pris possession de l'un de vos proches, un ectoplasme récalcitrant vous mène la vie dure ? N'hésitez plus et faites appel à notre équipe d'experts !").photo.attach(io: cat3, filename: 'exorc.jpg', content_type: 'image/png')
+Category.create(name: "Désinfection", description: "Une entité Maléfique a pris possession de l'un de vos proches, un ectoplasme récalcitrant vous mène la vie dure ? N'hésitez plus et faites appel à notre équipe d'experts !").photo.attach(io: cat3, filename: 'exorc.png', content_type: 'image/png')
 puts"Category created"
 
 #   Création des specialistes
@@ -18,25 +18,25 @@ Specialist.destroy_all
 lawyer_id = Category.find_by_name("Service Juridique").id
 
 #Milton
-# spl1 = URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615965749/p8x49gzjy1yvilebkjx5fyx3efj7.png")
-# spl1bis = URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615966234/qp03kps7klu207tixmlh9c6oxo19.png")
+spl1 = URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615965749/p8x49gzjy1yvilebkjx5fyx3efj7.png")
+spl1bis = URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615966234/qp03kps7klu207tixmlh9c6oxo19.png")
 
-# lawyer1 = Specialist.create(firstname: "John", lastname: "Milton", profession: "Juriste",
-# description: "Selon la croyance chrétienne traditionnelle sur la sorcellerie, le pacte est noué entre une personne et Satan (ou un moindre démon). La personne offre son âme en échange de faveurs diaboliques. Ces faveurs varient selon les contes, mais tendent à inclure la jeunesse, la connaissance, la richesse, la renommée, et/ou le pouvoir.", category_id: lawyer_id)
-# lawyer1.photos.attach(io: spl1, filename: 'milton.jpg', content_type: 'image/jpg')
-# lawyer1.photos.attach(io: spl1bis, filename: 'milton2.jpg', content_type: 'image/jpg')
-# puts"..."
-# #Belzeboss
-# spl2 = URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615966234/ju343ib9u6sayr4k8eberngr0ufn.jpg")
-# spl2bis = URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615919679/v3iy8t50h5p6jzkfsyh5qpyadg3q.jpg")
+lawyer1 = Specialist.create(firstname: "John", lastname: "Milton", profession: "Juriste",
+description: "Selon la croyance chrétienne traditionnelle sur la sorcellerie, le pacte est noué entre une personne et Satan (ou un moindre démon). La personne offre son âme en échange de faveurs diaboliques. Ces faveurs varient selon les contes, mais tendent à inclure la jeunesse, la connaissance, la richesse, la renommée, et/ou le pouvoir.", category_id: lawyer_id)
+lawyer1.photos.attach(io: spl1, filename: 'milton.png', content_type: 'image/png')
+lawyer1.photos.attach(io: spl1bis, filename: 'milton2.png', content_type: 'image/png')
+puts"..."
+#Belzeboss
+spl2 = URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615966234/ju343ib9u6sayr4k8eberngr0ufn.jpg")
+spl2bis = URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615919679/v3iy8t50h5p6jzkfsyh5qpyadg3q.jpg")
 
-# lawyer2 = Specialist.create(firstname: "BelzeBoss", lastname: "", profession: "Juriste",
-# description: "Yes, you are stuck, so out of luck, Now I'm complete and my jock you will pluck, This world will be mine and you're first in line
-# You brought me the pick and now you shall both die!", category_id: lawyer_id)
-# lawyer2.photos.attach(io: spl2, filename: 'belze.jpg', content_type: 'image/jpg')
-# lawyer2.photos.attach(io: spl2bis, filename: 'boss.jpg', content_type: 'image/jpg')
+lawyer2 = Specialist.create(firstname: "BelzeBoss", lastname: "", profession: "Juriste",
+description: "Yes, you are stuck, so out of luck, Now I'm complete and my jock you will pluck, This world will be mine and you're first in line
+You brought me the pick and now you shall both die!", category_id: lawyer_id)
+lawyer2.photos.attach(io: spl2, filename: 'belze.jpg', content_type: 'image/jpg')
+lawyer2.photos.attach(io: spl2bis, filename: 'boss.jpg', content_type: 'image/jpg')
 
-# puts"Lawyers created"
+puts"Lawyers created"
 
 #   Création du service médical
 doctor_id = Category.find_by_name("Expertise médicale").id
@@ -46,7 +46,7 @@ spd1bis =  URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v16159663
 doctor1 = Specialist.create(firstname: "Hannibal", lastname: "Lecter", profession: "Psychiatre",
 description: "Offrez-vous un moment de pure introspection, une plongée vivifiante et gustative au sein de votre inconscient le plus profond, le tout dans une ambiance tamisée et cosy. Attention, soyez à l'heure et soignez votre présentation. Le Docteur est un brin à cheval sur les règles de politesse.
 Bon à savoir : Si Hannibal souhaite vous inviter à dîner hors de nos locaux, refusez poliment et prévenez immédiatement Clarisse du S.A.V", category_id: doctor_id)
-doctor1.photos.attach(io: spd1, filename: 'hannibal.jpg', content_type: 'image/jpg')
+doctor1.photos.attach(io: spd1, filename: 'hannibal.png', content_type: 'image/png')
 doctor1.photos.attach(io: spd1bis, filename: 'lecter.jpg', content_type: 'image/jpg')
 puts"..."
 
@@ -57,7 +57,7 @@ spd2 =  URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615966459/
 spd2bis =  URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v1615966460/z8has8vfssxgsfdcw3ejwv6badj5.jpg")
 doctor2 = Specialist.create(firstname: "Annie", lastname: "Wilkes", profession: "Infirmière",
 description: "Petite cure de neuroleptiques ou simple besoin de vitamines après un Exorcisme un brin traumatisant ? Pas de panique, notre infirmière en chef, Annie, est là pour vous administrer le bon remède. Véritable virtuose de la seringue et spécialiste des articulations, Annie et son équipe prendront soin de vous comme il se doit. Bon à savoir: Caractère un peu soupe au lait et un brin sensible, déteste les spoilers sur sa série préférée", category_id: doctor_id)
-doctor2.photos.attach(io: spd2, filename: 'annie.jpg', content_type: 'image/jpg')
+doctor2.photos.attach(io: spd2, filename: 'annie.png', content_type: 'image/png')
 doctor2.photos.attach(io: spd2bis, filename: 'wilkes.jpg', content_type: 'image/jpg')
 
 puts"Doctors created"
@@ -70,7 +70,7 @@ spp1bis =  URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v16159667
 priest1 = Specialist.create(firstname: "Père", lastname: "Karras", profession: "Exorciste",
 description: "Bien que d'apparence rustique et un peu austère, le Père Karras saura vous surprendre par sa coolitude extrème et son flegme rassurant.
 Véritable légende vivante, le Père Karras affiche un tableau de chasse impressionnant : Pazuzu, Bélial, Dark Vador, Maître Gims...Souvent copié, jamais égalé. Il est le roi de l'Exorcisme vintage.Bon à savoir : A tendance à se défenestrer pour conclure ses rituels. Pas d'inquiétude, c'est sa manière à lui de fêter ses victoires...", category_id: priest_id)
-priest1.photos.attach(io: spp1, filename: 'pere.jpg', content_type: 'image/jpg')
+priest1.photos.attach(io: spp1, filename: 'pere.png', content_type: 'image/png')
 priest1.photos.attach(io: spp1bis, filename: 'karras.jpg', content_type: 'image/jpg')
 puts"..."
 
@@ -80,7 +80,7 @@ spp2bis =  URI.open("https://res.cloudinary.com/dsmxleyf5/image/upload/v16159665
 priest2 = Specialist.create(firstname: "Papa", lastname: "Emeritus", profession: "Exorciste",
 description: "Sa grande connaissance des rituels sataniques et du bestiaire démoniaque font de lui un Exorciste peu académique, mais d'une efficacité redoutable. Affichant un look glam et décomplexé, Papa Emeritus, jamais à cour de punchlines savoureuses, allie à la fois puissance et grâce pour un Exorcisme Premium au taux de réussite impressionnant.</p>
  Bon à savoir :A parfois tendance à sympathiser avec l'entité démoniaque... surprenant mais toujours hautement festif.", category_id: priest_id)
-priest2.photos.attach(io: spp2, filename: 'papa.jpg', content_type: 'image/jpg')
+priest2.photos.attach(io: spp2, filename: 'papa.png', content_type: 'image/png')
 priest2.photos.attach(io: spp2bis, filename: 'emeritus.jpg', content_type: 'image/jpg')
 
 puts"Priests created"

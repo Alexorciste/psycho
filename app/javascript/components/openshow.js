1f34a9db96c38657b4b7d1a22c62cc41a85887eb
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2021-04-01 21:16:34 +0200

    jsofzedead

--- app/javascript/components/openshow.js
+++ app/javascript/components/openshow.js
@@ -1,15 +1,30 @@
 const openNav = () => {
    
-   const show = document.querySelector(".specialists")
-    const cardOfDead = document.querySelector(".cardbox");
+   const leftPannel = document.querySelectorAll(".specialist-pannel")
+   const cardOfDead = document.querySelectorAll(".avatar-specialist")
+   
+  
+    cardOfDead.forEach((specialist) => {
+        
+        const testId = document.getElementById(`${specialist.id}`)
+        testId.addEventListener("click", (event) => {
+          
+          const testPannel = document.getElementById(`spe${specialist.id}`)
+          testPannel.classList.toggle("active");
+          
+          if((specialist.id)%2 == 0) {
+             
+            testPannel.style.right ="0"
+         } else {
+            testPannel.style.left="0"
+         }
+         
+       });
+
+
+    })
     
   
-  
-    cardOfDead.addEventListener("click", (event) => {
-      console.log(event)
-      show.classList.toggle("active");
-  });
-  
-  };
+};
   
   export { openNav };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,12 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import {openNav} from '../components/openshow';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
+  openNav();
   // Call your functions here, e.g:
   // initSelect2();
  
